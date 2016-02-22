9e4a0139a6cff955352ae51828a8394815662696
kathybeatz <kathleeng.lopez@gmail.com>
2016-02-22 14:52:46 -0800

    Figured out syntax error for Server Spec and started the server

--- test/ServerSpec.js
+++ test/ServerSpec.js
@@ -8,7 +8,7 @@ var Links = require('../app/collections/links');
 var Link = require('../app/models/link');
 
 /************************************************************/
-// Mocha doesn't have a way to designate pending beforero blocks.
+// Mocha doesn't have a way to designate pending before blocks.
 // Mimic the behavior of xit and xdescribe with xbeforeEach.
 // Remove the 'x' from beforeEach block when working on
 // authentication tests.
@@ -71,7 +71,7 @@ describe('', function() {
   describe('Link creation:', function(){
 
     var requestWithSession = request.defaults({jar: true});
-    xbeforeEach(function(done){      // create a user that we can then log-in with
+    beforeEach(function(done){      // create a user that we can then log-in with
       new User({
           'username': 'Phillip',
           'password': 'Phillip'
@@ -220,7 +220,7 @@ describe('', function() {
 
   }); // 'Link creation'
 
-    xbeforeEach(function(done){('Privileged Access:', function(){
+    beforeEach( function(done){ ('Privileged Access:', function() {
 
     it('Redirects to login page if a user tries to access the main page and is not signed in', function(done) {
       request('http://127.0.0.1:4568/', function(error, res, body) {
@@ -245,7 +245,7 @@ describe('', function() {
 
   }); // 'Priviledged Access'
 
-  xdescribe('Account Creation:', function(){
+  describe('Account Creation:', function(){
 
     it('Signup creates a user record', function(done) {
       var options = {
@@ -293,7 +293,7 @@ describe('', function() {
 
   }); // 'Account Creation'
 
-  xdescribe('Account Login:', function(){
+  describe('Account Login:', function(){
 
     var requestWithSession = request.defaults({jar: true});
 
@@ -339,5 +339,7 @@ describe('', function() {
     });
 
   }); // 'Account Login'
+
+});
 
 });
